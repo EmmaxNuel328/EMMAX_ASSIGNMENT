e53f5eea746bea6fd136a9a7c0cf7b84b1e5e742
Emmanuel <olatunjie335@gmail.com>
2025-09-24 08:37:19 +0100

    Add Night_snacks1.java

---
+++ Night_snacks1.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Night_snacks1{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter father's age:");
+int father = input.nextInt();
+System.out.print("Enter son's age:");
+int son = input.nextInt();
+while(father <=  80 ){
+father++;
+while(son <= 80){
+son++;
+while(father / son == 2){
+int dad = father / son;
+
+
+}
+}
+}
+}
