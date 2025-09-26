a4af5a8d44d5be39ea98a13a8f0d2637db47516b
Emmanuel <olatunjie335@gmail.com>
2025-09-26 12:03:46 +0100

    Add TaskFive.java

---
+++ GATE 1/TaskFive.java
@@ -0,0 +1,12 @@
+public class TaskFive{
+public static void main(String[] args){
+int number = 0;
+int count = 0;
+while(number < 10){
+number++;
+if(number % 4 == 0)
+  System.out.print(" "+number+number+number+number+number);
+
+}
+}
+}
