f4a7544c4aed9506f4ea0c4db01e931a55c43bc1
Emmanuel <olatunjie335@gmail.com>
2025-09-26 12:05:43 +0100

    Add TaskSeven.java

---
+++ GATE 1/TaskSeven.java
@@ -0,0 +1,11 @@
+public class TaskSeven{
+public static void main(String[] args){
+int number = 0;
+//int sum = 0;
+while(number < 10){
+number++;
+if(number % 4 == 0)
+System.out.println((number)+(number*number)+(number*number*number)+(number*number*number*number)+(number*number*number*number*number));
+}
+}
+}
