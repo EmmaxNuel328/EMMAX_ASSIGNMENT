80a300ebdb715835f076ec48c3c00c6aae9c321a
Emmanuel <olatunjie335@gmail.com>
2025-09-29 08:15:56 +0100

    Add Nokia2.java file

---
+++ GATE 1/Nokia2.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+public class Nokia2{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+  System.out.println("1: Phone book"); 
+  System.out.println("2: Messages");
+  System.out.println("3: Chat");
+  System.out.println("4: Call register");
+  System.out.println("5: Tones");
+  System.out.println("6: Settings");
+  System.out.println("7: Call divert");
+  System.out.println("8: Games");
+  System.out.println("9: Calculator");
+  System.out.println("10 Reminders");
+  System.out.println("11: Clock");
+  System.out.println("12: Profiles");
+  System.out.println("13: SIM services");
+  System.out.print("");
+  int collect1 = input.nextInt();
+  switch(collect1){
+case 1->{
+System.out.println("1.Search");
+  System.out.println("2.Service Nos.");
+  System.out.println("3.Add name");
+  System.out.println("4.Erase");
+  System.out.println("5.Edit");
+  System.out.println("6.Assign tone");
+  System.out.println("7.Send b'card");
+  System.out.println("8.Options");
+  System.out.println("9.Speed dials");
+  System.out.println("10.Voice tags");
+  System.out.println("0.Back");
+  System.out.print("");
+ int collect2 = input.nextInt();
+if(collect2 == 0){
+System.out.println("1: Phone book"); 
+  System.out.println("2: Messages");
+  System.out.println("3: Chat");
+  System.out.println("4: Call register");
+  System.out.println("5: Tones");
+  System.out.println("6: Settings");
+  System.out.println("7: Call divert");
+  System.out.println("8: Games");
+  System.out.println("9: Calculator");
+  System.out.println("10 Reminders");
+  System.out.println("11: Clock");
+  System.out.println("12: Profiles");
+  System.out.println("13: SIM services");
+  }
+if(collect2 ==8){
+System.out.println("1.Type of view");
+System.out.println("2.Memory status");
+System.out.println("0.Back");
+System.out.print("");
+int collect3 = input.nextInt();
+if(collect3 == 1){
+System.out.println("Type of view");
+System
+}
+else if(collect3 == 2){
+System.out.println("Memory status");
+}
+else if(collect3 == 0){
+System.out.println("1.Search");
+  System.out.println("2.Service Nos.");
+  System.out.println("3.Add name");
+  System.out.println("4.Erase");
+  System.out.println("5.Edit");
+  System.out.println("6.Assign tone");
+  System.out.println("7.Send b'card");
+  System.out.println("8.Options");
+  System.out.println("9.Speed dials");
+  System.out.println("10.Voice tags");
+  System.out.println("0.Back");
+  System.out.print("");
+ int back = input.nextInt();
+if(back== 0){
+System.out.println("1: Phone book"); 
+  System.out.println("2: Messages");
+  System.out.println("3: Chat");
+  System.out.println("4: Call register");
+  System.out.println("5: Tones");
+  System.out.println("6: Settings");
+  System.out.println("7: Call divert");
+  System.out.println("8: Games");
+  System.out.println("9: Calculator");
+  System.out.println("10 Reminders");
+  System.out.println("11: Clock");
+  System.out.println("12: Profiles");
+  System.out.println("13: SIM services");
+}
+
+
+}
+}
+}
+}
+
+}
+}
