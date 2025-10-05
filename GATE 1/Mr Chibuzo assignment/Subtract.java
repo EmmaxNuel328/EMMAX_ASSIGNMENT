f13697cd4a2587f916b1a6f0bd4de7fe965daad2
olatunji Emmanuel <olatunjie355@gmail.com>
2025-10-05 19:27:03 +0100

    Add Subtract.java file

---
+++ GATE 1/Mr Chibuzo assignment/Subtract.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Subtract{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.print("Enter first number: ");
+int numbers = input.nextInt();
+System.out.print("Enter second number: ");
+int integers = input.nextInt();
+System.out.print(Kata.Subtract(numbers,integers));
+}
+}
