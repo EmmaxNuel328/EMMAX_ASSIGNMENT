1a6f7f4047d0e6f484d2c1b14867c40e7d872278
olatunji Emmanuel <olatunjie355@gmail.com>
2025-10-06 08:32:55 +0100

    Add Kata.java file

---
+++ GATE 1/Mr Chibuzo assignment/Kata.java
@@ -0,0 +1,61 @@
+public class Kata{
+public static boolean isEven(int number){
+int numbers = number;
+if(numbers % 2 == 0){
+return true;
+}
+else{
+return false;
+}
+}
+public static int Subtract(int numbers,int integers){
+int number1 = numbers;
+int number2 = integers;
+int number3 = -1;
+if(number1 >= number2){
+return number3*(number1-number2);
+}
+else{
+return number3*(number1-number2);
+}
+}
+public static float Divide(int number, int numbers){
+float first = number;
+float second = numbers;
+if(second == 0){
+return 0;
+}
+else{
+return first/second;
+}
+}
+public static int Factor(int number){
+int one = number;
+int count = 1;
+int sum = 0;
+for(count = 1;count <= one ;count++){
+if(one%count==0){
+sum = sum + 1;
+}
+}
+return sum;
+}
+public static int SquareOf(int number1){
+int integer = number1;
+return integer*integer;
+}1
+public static boolean isSquare(int square){
+int integers = square;
+int count = 1;
+int sum = 0;
+for(count = 1;count <= integers ;count++){
+ifintegers%count==0){
+sum = sum + 1;
+if(sum%2!=0){
+return true;
+}
+return false; 
+}
+
+}  
+}
