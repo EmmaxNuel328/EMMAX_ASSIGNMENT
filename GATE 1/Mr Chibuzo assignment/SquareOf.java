68d638672bac1fd12c90a9f74e7fb4b4ab62c127
olatunji Emmanuel <olatunjie355@gmail.com>
2025-10-06 08:31:14 +0100

    Add Square.java file

---
+++ GATE 1/Mr Chibuzo assignment/SquareOf.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SquareOf{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int number1 = input.nextInt();
+System.out.print("The square of " + " " + number1  +  " " + "is" + " " +    Kata.SquareOf(number1 ));
+}
+}
